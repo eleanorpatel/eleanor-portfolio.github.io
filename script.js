3721ac10b8c80f41f78ecd525fe291378af0e2e7
eleanorpatel <eleanor.v.patel@gmail.com>
2026-04-27 18:21:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ const changeimage
 let displayState = 0;
 
 function changeProject;
-  
+  desctext.textContent = "This is the description for project 1"
+  var = "project"+ (displayState+1).twoString()
