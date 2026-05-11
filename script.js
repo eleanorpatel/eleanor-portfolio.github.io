03fe8149617b174d81a0f00196cb80821bcaced6
eleanorpatel <eleanor.v.patel@gmail.com>
2026-05-11 17:57:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const description
 const name
 const desctext
 const changeimage
+const next 
 
 let displayState = 0;
 
 function changeProject;
   desctext.textContent = "This is the description for project 1"
-  var = "project"+ (displayState+1).twoString()
+  let var = "project"+ (displayState+1).twoString()
+  let varnext =
